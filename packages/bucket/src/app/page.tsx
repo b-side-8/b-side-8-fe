de167952544a4ab0d19636797210a2b6958ff117
joonhyuk <anshqhsh.dev@gmail.com>
2023-06-06 22:09:21 +0900

    chore: typograph 예시 - 추후 storybook을 사용하는 것도 좋을것 같음

--- packages/bucket/src/app/page.tsx
+++ packages/bucket/src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from 'next/image';
+import Typograph from '@/components/typograph';
 
 export default function Home() {
-  return <div>bucket</div>;
+  return (
+    <div>
+      <Typograph variant='h1'>h1</Typograph>
+      <Typograph variant='h2'>h2</Typograph>
+      <Typograph variant='body1'>body1</Typograph>
+      <Typograph variant='title1'>title1</Typograph>
+      <Typograph variant='span'>span</Typograph>
+    </div>
+  );
 }
