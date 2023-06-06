c254b7b89d06b7422e65ea2c3c64c1c1f758f7d0
joonhyuk <anshqhsh.dev@gmail.com>
2023-06-06 17:36:05 +0900

    chore: next.config 수정

--- packages/bucket/next.config.js
+++ packages/bucket/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   compress: true, // 파일압축 웹 로딩속도 향상
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
