150db1d98595ce99dd97a87beab5f08395fb2f1f
anshqhsh <anshqhsh.dev@gmail.com>
2023-07-14 00:25:36 +0900

    feat: typograph import error 수정 & title값 변경

--- packages/bucket/src/components/typograph/index.tsx
+++ packages/bucket/src/components/typograph/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import styles from './Typograph.module.scss';
+import styles from './typograph.module.scss';
 import classNames from 'classnames';
 
 interface ITypographProps {
