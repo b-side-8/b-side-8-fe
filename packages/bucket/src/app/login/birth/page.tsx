ec62d35e28589f7cc1ca80cf9b375b71fc5da824
anshqhsh <anshqhsh.dev@gmail.com>
2023-07-16 00:55:02 +0900

    feat: birth 관련 zustand 추가

--- packages/bucket/src/app/login/birth/page.tsx
+++ packages/bucket/src/app/login/birth/page.tsx
@@ -8,30 +8,72 @@ import { useEffect, useRef, useState } from 'react';
 import Button from '@/components/Buttons';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import styles from './birth.module.scss';
+import userInfoStore from '@/zustand/userInfoStore';
+
+const useUserInfoNickName = () => {
+  return userInfoStore((store) => ({
+    userInfo: store.userInfo,
+    setBirth: store.setBirth,
+  }));
+};
 
 function NcikName() {
+  const [birthInput, setBirthInput] = useState({
+    year: '',
+    month: '',
+    day: '',
+  });
+  const { userInfo, setBirth } = useUserInfoNickName();
+
   const router = useRouter();
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBirthInput({
+      ...birthInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onClickNext = () => {
+    const { year, month, day } = birthInput;
+    setBirth(year, month, day);
+    router.push('/login/gender');
+  };
 
   return (
     <div className={styles.birthWrapper}>
       <div>
+        {JSON.stringify(userInfo)}
         <TopNavigateBar />
         <Typograph className={styles.birthTitle} variant='title1'>
           생년월일이 궁금해요.
         </Typograph>
         <div className={styles.textFieldWrapper}>
-          <TextField type='number' size='small' placeholder='YYYY' />
-          <TextField type='number' size='small' placeholder='MM' />
-          <TextField type='number' size='small' placeholder='DD' />
+          <TextField
+            type='number'
+            size='small'
+            placeholder='YYYY'
+            name='year'
+            onChange={onChangeInput}
+          />
+          <TextField
+            type='number'
+            size='small'
+            placeholder='MM'
+            name='month'
+            onChange={onChangeInput}
+          />
+          <TextField
+            type='number'
+            size='small'
+            placeholder='DD'
+            name='day'
+            onChange={onChangeInput}
+          />
         </div>
       </div>
       <div className={styles.buttonWrapper}>
-        <Button
-          size='large'
-          onClick={() => {
-            router.push('/login/gender');
-          }}
-        >
+        <Button size='large' onClick={onClickNext}>
           다음
         </Button>
       </div>
