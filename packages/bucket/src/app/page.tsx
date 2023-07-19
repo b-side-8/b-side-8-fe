e29450e6e0301adc78844b57f1521e051ead715c
anshqhsh <anshqhsh.dev@gmail.com>
2023-07-19 16:39:06 +0900

    feat: 카카오 로그인 수정

--- packages/bucket/src/app/page.tsx
+++ packages/bucket/src/app/page.tsx
@@ -5,13 +5,6 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
-  const onClick = async () => {
-    const res = await fetch(
-      'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=20b68e93f8a5a97696de7503ec0e8315&redirect_uri=http%3A%2F%2F49.50.160.93%3A7070%2Foauth%2Fkakao'
-    );
-    console.log(res.url);
-    router.push(res.url);
-  };
 
   return (
     <div>
@@ -20,14 +13,6 @@ export default function Home() {
       <Typograph variant='body1'>body1</Typograph>
       <Typograph variant='title1'>title1</Typograph>
       <Typograph variant='span'>span</Typograph>
-      <button onClick={onClick}>요청</button>
-      <a
-        href={
-          'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=20b68e93f8a5a97696de7503ec0e8315&redirect_uri=http%3A%2F%2F49.50.160.93%3A7070%2Foauth%2Fkakao'
-        }
-      >
-        카카오 로그인
-      </a>
     </div>
   );
 }
