a76a39732ee7a149086c27063fc97df8cfa3d931
anshqhsh <anshqhsh.dev@gmail.com>
2023-08-06 22:46:04 +0900

    chore: setting페이지 topnavigationBar 추가

---
+++ packages/bucket/src/app/setting/page.tsx
@@ -0,0 +1,9 @@
+import TopNavigateBar from '@/components/TopNavigateBar';
+
+export default function Setting() {
+  return (
+    <div>
+      <TopNavigateBar type="settingNavigator" />
+    </div>
+  );
+}
